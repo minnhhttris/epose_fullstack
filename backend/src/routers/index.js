4871704cea9c1ec6865ce418e8ec66d2c39d6ba2
minnhhttris <ntmtri02@gmail.com>
2024-09-07 07:59:01 +0700

    create itemSizes service

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const userRouter = require('./User/user.router');
+const storeRouter = require('./Store/store.router');
+const clothesRouter = require('./Clothes/clothes.router');
+const itemSizesRouter = require('./ItemSizes/itemSizes.router');
 
 const router = express.Router();
 
 router.use('/users', userRouter);
+router.use('/stores', storeRouter);
+router.use('/clothes', clothesRouter);
+router.use('/item-sizes', itemSizesRouter);
 
 module.exports = router;
