a5fa2b60d41b5b6be1d9ed3e2626adc8aee54136
minnhhttris <ntmtri02@gmail.com>
2024-09-29 18:36:55 +0700

    config app UI

--- epose_app/lib/features/main/nav/bill/presentation/widgets/bill_appbar_widget.dart
+++ epose_app/lib/features/main/nav/bill/presentation/widgets/bill_appbar_widget.dart
@@ -1,4 +1,3 @@
-import 'package:epose_app/core/ui/widgets/appbar/appbar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -8,48 +7,39 @@ import '../../../../../../core/configs/app_dimens.dart';
 import '../../../../../../core/configs/app_images_string.dart';
 import '../../../../../../core/routes/routes.dart';
 
-class ProfileAppbar extends StatelessWidget implements PreferredSizeWidget {
-  const ProfileAppbar({super.key});
+class BillAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const BillAppbar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(
+      appBar: AppBar(
         actions: [
           IconButton(
-            icon: const Icon(
-              Icons.chat,
+            icon: SvgPicture.asset(
+              AppImagesString.eNotify,
+              width: 26,
+              height: 26,
               color: AppColors.black,
-              size: AppDimens.textSize28,
             ),
             onPressed: () {
-              Get.toNamed(Routes.mesage);
+              Get.toNamed(Routes.notify);
             },
           ),
           IconButton(
-             icon: SvgPicture.asset(
-            AppImagesString.eBagShopping,
-            width: 26,
-            height: 26,
-            color: AppColors.black,
-          ),
+            icon: SvgPicture.asset(
+              AppImagesString.eBagShopping,
+              width: 26,
+              height: 26,
+              color: AppColors.black,
+            ),
             onPressed: () {
               Get.toNamed(Routes.bagShopping);
             },
           ),
-          IconButton(
-            icon: const Icon(
-              Icons.settings,
-              color: AppColors.black,
-              size: AppDimens.textSize28,
-            ),
-            onPressed: () {
-              Get.toNamed(Routes.accountSetting);
-            },
-          ),
           const SizedBox(width: AppDimens.columnSpacing),
         ],
-      )
+      ),
     );
   }
 
--- epose_app/lib/features/main/nav/clothes/presentation/page/clothes_page.dart
+++ epose_app/lib/features/main/nav/clothes/presentation/page/clothes_page.dart
@@ -1,9 +1,12 @@
+import 'package:epose_app/core/ui/widgets/button/button_widget.dart';
+import 'package:epose_app/core/ui/widgets/text/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../../../core/configs/app_colors.dart';
 import '../../../../../../core/configs/app_dimens.dart';
 import '../controller/clothes_controller.dart';
+import '../widgets/clothes_appbar_widget.dart';
 import '../widgets/clothes_card_widget.dart';
 import '../widgets/filter_button.dart';
 
@@ -13,159 +16,172 @@ class ClothesPage extends GetView<ClothesController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const TextField(
-          decoration: InputDecoration(
-            hintText: 'Tìm sản phẩm...',
-            border: InputBorder.none,
-            prefixIcon: Icon(Icons.search, color: AppColors.primary),
-          ),
+      appBar: const ClothesAppbar(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            titleAndButtonFilter(),
+            listClothes(),
+          ],
         ),
-        backgroundColor: Colors.white,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.notifications_none, color: AppColors.black),
+      ),
+    );
+  }
+
+  Padding titleAndButtonFilter() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            'Xu hướng',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: AppDimens.textSize18,
+              color: AppColors.black,
+            ),
           ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.shopping_cart_outlined,
-                color: AppColors.black),
+          FilterButton(
+            onTap: controller.toggleFilterMenu,
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Hàng chứa tiêu đề "Xu hướng" và nút lọc
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Xu hướng',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: AppDimens.textSize16,
-                    color: AppColors.black,
-                  ),
-                ),
-                FilterButton(
-                  onTap: controller.toggleFilterMenu,
-                ),
-              ],
+    );
+  }
+
+   Widget listClothes() {
+    return Stack(
+      children: [
+        Obx(
+          () => GridView.builder(
+            shrinkWrap: true,
+            physics:
+                const NeverScrollableScrollPhysics(), // Vô hiệu hóa cuộn riêng của GridView
+            padding: const EdgeInsets.all(8.0),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+              childAspectRatio: 0.6,
             ),
+            itemCount: controller.clothesList.length,
+            itemBuilder: (context, index) {
+              final item = controller.clothesList[index];
+              return ClothesCard(
+                imageUrl: item['imageUrl'] ?? '',
+                storeName: item['storeName'] ?? '',
+                price: item['price'] ?? '',
+                productName: item['productName'] ?? '',
+                tags: item['tags'] ?? '',
+              );
+            },
           ),
-          Expanded(
-            child: Stack(
-              children: [
-                Obx(
-                  () => GridView.builder(
-                    padding: const EdgeInsets.all(8.0),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 8.0,
-                      mainAxisSpacing: 8.0,
-                      childAspectRatio: 0.6,
-                    ),
-                    itemCount: controller.clothesList.length,
-                    itemBuilder: (context, index) {
-                      final item = controller.clothesList[index];
-                      return ClothesCard(
-                        imageUrl: item['imageUrl'] ?? '',
-                        storeName: item['storeName'] ?? '',
-                        price: item['price'] ?? '',
-                        productName: item['productName'] ?? '',
-                        tags: item['tags'] ?? '',
-                      );
-                    },
-                  ),
-                ),
-                Obx(
-                  () => controller.isFilterMenuOpen.value
-                      ? Positioned(
-                          top: 0,
-                          right: 0,
-                          child: Container(
-                            width: Get.width * 0.6,
-                            height: Get.height,
-                            color: Colors.white.withOpacity(0.95),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+        Obx(
+          () => controller.isFilterMenuOpen.value
+              ? Positioned(
+                  top: 0,
+                  right: 0,
+                  child: Container(
+                    width: Get.width * 0.7,
+                    height: Get.height * 0.5,
+                    color: AppColors.primary2.withOpacity(0.90),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ListTile(
+                            title: const TextWidget(text: 'Giới tính'),
+                            trailing: const Icon(
+                              Icons.arrow_forward_ios,
+                              color: AppColors.primary,
+                            ),
+                            onTap: () {
+                              // Xử lý lọc theo giới tính
+                            },
+                          ),
+                          const Divider(
+                            color: AppColors.primary,
+                            thickness: 1,
+                          ),
+                          ListTile(
+                            title: const Text('Theo mùa'),
+                            trailing: const Icon(
+                              Icons.arrow_forward_ios,
+                              color: AppColors.primary,
+                            ),
+                            onTap: () {
+                              // Xử lý lọc theo mùa
+                            },
+                          ),
+                          const Divider(
+                            color: AppColors.primary,
+                            thickness: 1,
+                          ),
+                          ListTile(
+                            title: const Text('Màu sắc'),
+                            trailing: const Icon(
+                              Icons.arrow_forward_ios,
+                              color: AppColors.primary,
+                            ),
+                            onTap: () {
+                              // Xử lý lọc theo màu sắc
+                            },
+                          ),
+                          const Divider(
+                            color: AppColors.primary,
+                            thickness: 1,
+                          ),
+                          ListTile(
+                            title: const Text('Giá tiền'),
+                            trailing: const Icon(
+                              Icons.arrow_forward_ios,
+                              color: AppColors.primary,
+                            ),
+                            onTap: () {
+                              // Xử lý lọc theo giá tiền
+                            },
+                          ),
+                          const Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 5.0, vertical: 5.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                ListTile(
-                                  title: const Text('Giới tính'),
-                                  trailing: const Icon(Icons.arrow_forward_ios),
-                                  onTap: () {
-                                    // Xử lý lọc theo giới tính
-                                  },
-                                ),
-                                ListTile(
-                                  title: const Text('Theo mùa'),
-                                  trailing: const Icon(Icons.arrow_forward_ios),
-                                  onTap: () {
-                                    // Xử lý lọc theo mùa
-                                  },
-                                ),
-                                ListTile(
-                                  title: const Text('Màu sắc'),
-                                  trailing: const Icon(Icons.arrow_forward_ios),
-                                  onTap: () {
-                                    // Xử lý lọc theo màu sắc
-                                  },
-                                ),
-                                ListTile(
-                                  title: const Text('Giá tiền'),
-                                  trailing: const Icon(Icons.arrow_forward_ios),
-                                  onTap: () {
-                                    // Xử lý lọc theo giá tiền
-                                  },
+                                ButtonWidget(
+                                  width: 120,
+                                  height: 40,
+                                  ontap: controller.resetFilters,
+                                  backgroundColor: AppColors.white,
+                                  text: 'Thiết lập lại',
+                                  textColor: AppColors.black,
+                                  borderRadius: 5,
                                 ),
                                 const Spacer(),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 16.0, vertical: 8.0),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      ElevatedButton(
-                                        onPressed: controller.resetFilters,
-                                        style: ElevatedButton.styleFrom(
-                                          backgroundColor: AppColors.primary
-                                              .withOpacity(
-                                                  0.1), // Màu nền nhẹ nhàng
-                                        ),
-                                        child: const Text(
-                                          'Thiết lập lại',
-                                          style: TextStyle(
-                                              color: AppColors
-                                                  .primary), // Màu chữ của nút
-                                        ),
-                                      ),
-                                      ElevatedButton(
-                                        onPressed: controller.applyFilters,
-                                        style: ElevatedButton.styleFrom(
-                                          backgroundColor: AppColors.primary, // Màu nền nút
-                                        ),
-                                        child: const Text('Áp dụng'),
-                                      ),
-                                    ],
-                                  ),
+                                ButtonWidget(
+                                  width: 120,
+                                  height: 40,
+                                  ontap: controller.applyFilters,
+                                  backgroundColor: AppColors.primary,
+                                  text: 'Áp dụng',
+                                  textColor: AppColors.white,
+                                  borderRadius: 5,
                                 ),
                               ],
                             ),
                           ),
-                        )
-                      : const SizedBox.shrink(),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ), );
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              : const SizedBox.shrink(),
+        ),
+      ],
+    );
   }
 }
