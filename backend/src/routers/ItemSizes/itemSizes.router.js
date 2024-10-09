ff685f939a4586c1a4cbe7f8a7c908d84117b6fc
minnhhttris <ntmtri02@gmail.com>
2024-10-10 01:51:27 +0700

    delete itemsize

--- backend/src/routers/ItemSizes/itemSizes.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const ItemSizesController = require("../../controllers/ItemSizes/itemSizes.controller");
-const { verifyToken } = require("../../middlewares/verifyToken");
-const router = express.Router();
-
-router.put("/:idItem", verifyToken, ItemSizesController.updateSizeQuantity);
-router.get("/:idItem", ItemSizesController.getItemSizes);
-
-module.exports = router;
